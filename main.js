b73d23b6b44c619d12f1300af4d43c422a6efdaa
SH049 <146598693+SH049@users.noreply.github.com>
2023-12-16 14:52:08 +0900

    見た目の変更&小数点の制御

--- main.js
+++ main.js
@@ -2,25 +2,32 @@ $(document).ready(function() {
     const $button = $('.button');
     const $buttonAC = $('#AC')
     const $result = $('#result');
-    const $result2 =$('#result2');
+    const $result2 =$('#result2'); //計算用に演算子の後の数字を格納
     const $buttonElem = $('.button_elem');
     const $buttonEqual = $('#equal');
+    const $buttonDot = $('#dot')
     let num = 0; //計算用の値を格納する
     let elem = ""; //演算子のidを格納する
     let sum = 0; //計算結果を格納する
     let count = 0; //演算子のボタンを押した回数を数える
+    let countDot = 0; //小数点の回数を数える
     let numRear = 0; //演算子の後ろの数
 
+    
+    // 電卓に表示
     $button.on("click", function(){
         
-        if ($result.text() == 0) {
+        if ($result.text() == 0 && countDot < 1) { //0.1などは入力できるように
             $result.text($(this).text());
+            if ($result.text() == "00") { //00が入力された場合、0に変更
+                $result.text(0);
+            }
         } else {
             $result.append($(this).text());
         };
         if (elem != "") {
             $result2.append($(this).text());
-            num = $result2.text(); //演算子の後
+            num = $result2.text(); //演算子の後　表示はされない
         } else {
             num = $result.text(); //演算子の前
         }
@@ -29,40 +36,47 @@ $(document).ready(function() {
         console.log(Number(num)); //テスト用
     });
 
+    // 小数点の制御
+    $buttonDot.on("click", function(){
+        if (countDot >= 1) { //countが1以上
+            return;
+        }
+        if (elem != "") {
+            $result.append($(this).text());
+            $result2.append($(this).text()); //演算子の後 表示はされない
+        } else {
+            $result.append($(this).text()); //演算子の前
+        }
+        countDot += 1;
+    });
+
+
     //イメージ
     //演算子ボタンを押すと、numをなんらかの変数に代入して、numをゼロにする
     $buttonElem.on("click", function(){
-        $result2.text(''); // 2回目の計算にてresult2の初期化 効いてなさそう
+        $result2.text(''); // 2回目の計算にてresult2の初期化
         elem = ""; //2回目の計算にてelemの初期化
         if (count >= 1) { //countが1以上
             return;
         }
         sum = Number(num); //numの値をsumに代入
-        // if (sum == 0) {
-        //     sum += num;
-        //     $result.append($(this).text());
-        //     num = 0;
-        //     return;
-        // }
+        
         if (num != 0) {
         $result.append($(this).text()); //numが0の場合以外は演算子を表示
         } 
         elem = $(this).attr('id'); //演算子のidを取得
         num = 0; //numの値を0にする
-        //console.log(elem); //テスト用
-       count += 1; 
-        
+        count += 1; 
+        countDot = 0;
     });
 
     // イメージ
     // イコールを押すと、sumの値が出力される。
-    // 現時点でイコールを２回押してもうまく表示されない
     $buttonEqual.on("click", function(){
-        //console.log(sum) //テスト用
+        
         if (count != 0) { //連続でイコールを押したときはnumRearの値を更新しない
             numRear = Number(num);
         }
-        // sum = number1 + number2; //足し算
         
         switch(elem) {
             case "plus":
@@ -80,19 +94,18 @@ $(document).ready(function() {
         }
 
         $result.text(sum);
-        // elem = "";
         num = sum; //numにsumを代入
-        // $result2.text(''); result2を初期化
         count = 0; //countの初期化
+        countDot = 0;
     })
 
-
+    //出力のクリア
     $buttonAC.on("click", function(){
         $result.text(0);
         num = 0;
         sum = 0;
         elem = "";
         count = 0; //カウントの初期化
-        //console.log(Number(num)); //テスト用
-    })
+        countDot = 0;
+    });
 });
